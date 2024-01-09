156361ffe9db136f8d93e34903fa955065b63dbf
Iamsharath007 <sharathpournami@gmail.com>
2024-01-10 01:26:04 +0530

    first commint

---
+++ numpyarrays.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+# Some basic array functions
+np_array = np.arange(1, 11, 1)  # start , stop , step
+print(np_array)
+
+np_array = np.linspace(1, 10, 6)  # start, stop , number of elements
+print(np_array)
+
+print(np.zeros((3, 6)))  # row , column
+print(np.zeros((3, 6)).shape)
+
+print(np_array.reshape((2, 3)))
+print(np_array.ravel())
+
+# Some mathematical fucnitons
+np_array = np_array.reshape((3, 2))
+print(np_array.min())  # can also use max
+print(np_array.sum(axis=0))  # column wise sum
+print(np_array.sum(axis=1))  # row wise sum
+print(np.std(np_array))  # standard deviation
+
+a = np.zeros((2, 2))
+b = np.ones((2, 2))
+print(a + b + 3)
+print(a.dot(b))  # dot product
+
+
